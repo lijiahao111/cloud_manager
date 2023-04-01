30cff69f8db12927d18dae874967060e1e161a98
lijihao111 <912044062@qq.com>
2023-04-01 09:23:37 +0800

    添加多个service

--- order_service/src/main/java/cn/wq/web/OrderController.java
+++ order_service/src/main/java/cn/wq/web/OrderController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author chenchao
  * @Data 2023/3/29 12:48
@@ -22,5 +26,18 @@ public class OrderController {
         Order order = orderService.gerOrderByid(id);
         return  order;
 
+    }
+
+    public static void main(String[] args) {
+      String url ="jdbc:mysql://localhost:3306/nacos";//?characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useUnicode=true&useSSL=false&serverTimezone=UTC
+       String user ="root";
+        String passsword="000000";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, passsword);
+            System.out.println("connection = " + connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
