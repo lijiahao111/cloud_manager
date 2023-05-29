63644f0ba6aff9cec3b7c3e6ca8521053e06f2e6
li <912044062@qq.com>
2023-05-29 17:49:28 +0800

    es

---
+++ elk_search/src/main/java/cn/elk/ElkSearchApplication.java
@@ -0,0 +1,13 @@
+package cn.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkSearchApplication.class, args);
+    }
+
+}
---
+++ elk_search/src/test/java/cn/elk/ElkSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
